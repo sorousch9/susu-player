7d79652d807815694500a57253c8194b00113f9f
Sorousch <soroush.raw@gmail.com>
2023-03-25 11:57:51 +0100

    app component for sidebar and header updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
+import Discover from "./pages/Discover";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppBar, Drawer, makeStyles } from "@material-ui/core";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+const drawerWidth = 240;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    [theme.breakpoints.up("md")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    [theme.breakpoints.up("md")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    [theme.breakpoints.up("md")]: {
+      marginLeft: drawerWidth,
+    },
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Header />
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={classes.drawer}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <Sidebar />
+        </Drawer>
+        <main className={classes.content}>
+          <Routes>
+            <Route path="/" element={<Discover />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
