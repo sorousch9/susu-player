16365a2019e2c19c22bc7ab1c358f1c8d81fb594
Sorousch <soroush.raw@gmail.com>
2023-03-25 12:26:22 +0100

    constants as spotify created

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Box, Select, Typography } from "@material-ui/core";
+import { genres } from "../assets/constants";
 
 const Discover = () => {
   return (
-    <div>Discover</div>
-  )
-}
+    <Box display="flex" flexDirection="column">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mt={4}
+        mb={10}
+      >
+        <Typography variant="h4" component="h2" color="textPrimary">
+          Discover
+        </Typography>
+        <Select
+          native
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </Select>
+      </Box>
 
-export default Discover
+      <Box display="flex" flexWrap="wrap" justifyContent="flex-start">
+        song cart
+      </Box>
+    </Box>
+  );
+};
+
+export default Discover;
