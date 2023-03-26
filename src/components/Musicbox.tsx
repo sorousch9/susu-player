84be7487fdf2a0248b17fd04548da81f96ef1a2e
Sorousch <soroush.raw@gmail.com>
2023-03-26 15:00:16 +0200

    Add musicbox component with audio player and controls

--- src/components/Musicbox.tsx
+++ src/components/Musicbox.tsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
-import { Card, Col, Row, ProgressBar, Button } from "react-bootstrap";
+import { useState, useRef } from "react";
+import classes from "./musicbox.module.css";
+import {
+  Card,
+  Col,
+  Row,
+  ProgressBar,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
 
 const Musicbox = () => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const togglePlayPause = () => {
+  
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+const togglePlayPause = () => {
+  const audio = audioRef.current;
+  if (audio) {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
     setIsPlaying(!isPlaying);
-  };
+  }
+};
+
   return (
     <Card>
       <Row>
@@ -15,46 +35,47 @@ const Musicbox = () => {
             <Card.Subtitle className="mb-2 text-muted">
               Mac Miller
             </Card.Subtitle>
-            <Row className="align-items-center">
-              <Col>
-                <Button variant="outline-dark">
+            <div>
+              <ButtonGroup>
+                <Button className={classes.musicPlayerButton}>
                   <i className="bi bi-skip-backward" />
                 </Button>
-              </Col>
-              <Col>
-                <Button variant="outline-dark">
+
+                <Button className={classes.musicPlayerButton}>
                   <i className="bi bi-arrow-counterclockwise" />
                 </Button>
-              </Col>
-              <Col>
-                <Button variant="outline-dark" onClick={togglePlayPause}>
+
+                <Button
+                  className={classes.musicPlayerButton}
+                  onClick={togglePlayPause}
+                  active={isPlaying}
+                >
                   {isPlaying ? (
                     <i className="bi bi-pause" />
                   ) : (
                     <i className="bi bi-play" />
                   )}
                 </Button>
-              </Col>
-              <Col>
-                <Button variant="outline-dark">
+
+                <Button className={classes.musicPlayerButton}>
                   <i className="bi bi-arrow-clockwise" />
                 </Button>
-              </Col>
-              <Col>
-                <Button variant="outline-dark">
+
+                <Button className={classes.musicPlayerButton}>
                   <i className="bi bi-skip-forward" />
                 </Button>
-              </Col>
-            </Row>
-            <Row className="mt-2">
+              </ButtonGroup>
+            </div>
+            <div className="mt-2">
               <Col>
                 <audio
+                  ref={audioRef}
                   src="https://cdn.pixabay.com/download/audio/2022/03/25/audio_42b0dba7b5.mp3?filename=i-canx27t-fall-in-love-106865.mp3"
                   preload="metadata"
                 />
                 <ProgressBar variant="primary" now={60} />
               </Col>
-            </Row>
+            </div>
           </Card.Body>
         </Col>
         <Col xs={6} md={4}>
