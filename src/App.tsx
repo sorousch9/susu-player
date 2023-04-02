48bf319cca901d120fa13969dc793e0c90aa7075
Sorousch <soroush.raw@gmail.com>
2023-04-03 01:04:11 +0200

    Update App component to reflect changes in List component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,10 @@ import Discover from "./pages/Discover";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import { Container, Row, Col } from "react-bootstrap";
-import { useEffect, useState } from "react";
 import Player from "./components/Player";
-import { MusicType } from "./types/music";
-import axios from "axios";
 import Header from "./components/Header";
 function App() {
-  const [id, setId] = useState<string>("");
-  const [musics, setMusics] = useState<MusicType[]>([]);
 
-  useEffect(() => {
-    const fetchDataAsync = async () => {
-      const response = await axios.get<MusicType[]>(
-        "http://localhost:3009/musics"
-      );
-      if (response !== undefined) {
-        setMusics(response.data);
-      }
-    };
-    fetchDataAsync();
-  }, []);
 
   return (
     <BrowserRouter>
@@ -39,10 +23,7 @@ function App() {
                 path="/"
                 element={
                   <Discover
-                    id={id}
-                    setId={setId}
-                    musics={musics}
-                    setMusics={setMusics}
+              
                   />
                 }
               />
@@ -50,7 +31,7 @@ function App() {
           </Col>
         </Row>
         <Row className="player-wrapper">
-          <Player musics={musics} id={id} setId={setId} />
+          <Player />
         </Row>
       </Container>
     </BrowserRouter>
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,26 +1,40 @@
-import { List } from "../components/List";
 import { MusicType } from "../types/music";
-import { Col, Container, Row } from "react-bootstrap";
-interface Props {
-  setId: (e: string) => void;
-  id: string;
-  musics: MusicType[];
-  setMusics: (e: MusicType[]) => void;
-}
-const Discover = ({ id, setId, musics, setMusics }: Props) => {
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { addMusic } from "../redux/playerReducer";
+const Discover = () => {
+  const [musics, setMusics] = useState<MusicType[]>([]);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      const response = await axios.get<MusicType[]>(
+        "http://localhost:3009/musics"
+      );
+      if (response !== undefined) {
+        setMusics(response.data);
+      }
+    };
+    fetchDataAsync();
+  }, []);
   return (
     <Container>
       <Row className="discover">
         {musics.map((music) => (
           <Col xs={6} md={2} key={music.id}>
-            <List
-              img={music.album_img}
-              title={music.title}
-              audio={music.audio}
-              album={music.album}
-              setId={setId}
-              musicId={music.id}
-            />
+            <Card
+              onClick={() => {
+                addMusic(music);
+              }}
+              bg="transparent"
+            >
+              <Card.Img src={music.album_img} alt={music.title} />
+              <Card.Title>{music.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {music.album}
+              </Card.Subtitle>
+              <audio src={music.audio} />
+            </Card>
           </Col>
         ))}
       </Row>
