78051b5d39d19a8d02f20ede9075f1c4f39d01c8
Sorousch <soroush.raw@gmail.com>
2023-04-06 10:36:15 +0200

    footer created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,32 @@
+import { Container } from "react-bootstrap";
+
 export const Footer = () => {
-
-  return <div>Footer</div>;
-
+  return (
+    <Container className="footer">
+      <div className="tab1">
+        <div className="footer-more">
+          <h6>More</h6>
+          <p>Get Web App</p>
+          <p>Premium</p>
+          <p>help and Support</p>
+          <p>Advertise</p>
+          <p>Contact</p>
+        </div>
+        <div className="footer-follow">
+          <h6>Follow Us on</h6>
+          <p>Instagram</p>
+          <p>Youtube</p>
+          <p>Twitter</p>
+          <p>TikTok</p>
+          <p>Telegram</p>
+        </div>
+      </div>
+      <div className="tab2">
+        <span>© 2023 Soroush Safarkhah</span>
+        <span>Privacy Policy</span>
+        <span>Terms of Use</span>
+        <span>About</span>
+      </div>
+    </Container>
+  );
 };
