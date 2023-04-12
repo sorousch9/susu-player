e199dee1cbc4792cfe0ddc9bbd4c8f0c5556c078
Sorousch <soroush.raw@gmail.com>
2023-04-12 11:40:09 +0200

    reorganize PlayList page

--- src/pages/PlayList.tsx
+++ src/pages/PlayList.tsx
@@ -1,18 +1,45 @@
-import { useSelector } from "react-redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { MusicType } from "../types/music";
 import { RootState } from "../redux/store";
+import { addToPlaylist, removeFromPlaylist } from "../redux/playlistReducer";
+interface PlaylistProps {
+  musics?: MusicType[];
+}
 
-const PlayList = () => {
-  const playlist = useSelector((state: RootState) => state.playlist);
-  console.log(playlist.musics);
+const Playlist: React.FC<PlaylistProps> = ({ musics }) => {
+  const playlist = useSelector((state: RootState) => state.playlist.musics);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (musics) {
+      musics.forEach((music) => {
+        dispatch(addToPlaylist({ music }));
+      });
+    }
+  }, [dispatch, musics]);
+
+  const handleRemove = (id: string) => {
+    dispatch(removeFromPlaylist(id));
+  };
+
   return (
-    <div>
-      {playlist.musics.map((music) => (
-        <ul key={music.id}>
-          <li>{music.title}</li>
+    <div className="playlist">
+      <h2>Playlist</h2>
+      {playlist.length === 0 ? (
+        <p>Your playlist is empty</p>
+      ) : (
+        <ul>
+          {playlist.map((music) => (
+            <li key={music.id}>
+              {music.title} - {music.artist}{" "}
+              <button onClick={() => handleRemove(music.id)}>Remove</button>
+            </li>
+          ))}
         </ul>
-      ))}
+      )}
     </div>
   );
 };
 
-export default PlayList;
+export default Playlist;
