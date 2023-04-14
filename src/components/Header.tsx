cad4ba03bf854e76714b932407bc58cf9b94efda
Sorousch <soroush.raw@gmail.com>
2023-04-14 14:12:26 +0200

    Refactor search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { MusicType } from "../types/music";
+import { Link } from "react-router-dom";
 const Header = () => {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState<MusicType[]>([]);
+  const [query, setQuery] = useState<string>("");
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.target.value);
+  const handleSearch = async (searchQuery: string) => {
+    const response = await axios.get<MusicType[]>(
+      `http://localhost:3009/musics?q=${searchQuery}`
+    );
+    return response.data;
   };
+
+  useEffect(() => {
+    if (query !== "") {
+      handleSearch(query).then((searchResults) => {
+        setSearchValue(searchResults);
+      });
+    } else {
+      setSearchValue([]);
+    }
+  }, [query]);
 
   return (
     <Navbar className="header">
@@ -17,10 +34,24 @@ const Header = () => {
           <input
             type="text"
             placeholder="Search for Artists, Songs, or Podcasts..."
-            value={searchValue}
-            onChange={handleSearch}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
+
+          {searchValue.length >= 1 && (
+            <ul className="search-results">
+              {searchValue.map((music) => (
+                <li key={music.id}>
+                  <div className="result-container">
+                    <img src={music.album_img} alt={music.title}></img>
+                    <Link to={`/musics/${music.id}`}>{music.title}</Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+
         <Nav className="nav-drop">
           <NavDropdown
             title={<i className="bi bi-person-fill" />}
