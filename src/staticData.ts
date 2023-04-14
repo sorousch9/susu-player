1e1560c8ad77de4c34910175b78e8ea2effd076b
Sorousch <soroush.raw@gmail.com>
2023-04-14 13:07:11 +0200

    Add staticData.ts to store playlist data

---
+++ src/staticData.ts
@@ -0,0 +1,44 @@
+import todayHits from "./assets/todayHits.webp";
+import crush from "./assets/crush.webp";
+import dance from "./assets/dance.webp";
+import feelGood from "./assets/feelGood.webp";
+import relaxing from "./assets/relaxing.webp";
+import traffic from "./assets/traffic.webp";
+export const playlist = [
+  {
+    id: 1,
+    img: todayHits,
+    title: "Today Top Hits",
+    description: "The most popular songs of the day, updated regularly.",
+  },
+  {
+    id: 2,
+    img: crush,
+    title: "Crush",
+    description: "Love songs to make your heart skip a beat.",
+  },
+  {
+    id: 3,
+    img: dance,
+    title: "Dance",
+    description: "Upbeat tracks to get you moving and grooving.",
+  },
+  {
+    id: 4,
+    img: feelGood,
+    title: "Feel Good",
+    description: "Songs to put you in a happy and positive mood.",
+  },
+  {
+    id: 5,
+    img: relaxing,
+    title: "Relaxing",
+    description: "Calming music to help you unwind and destress.",
+  },
+  {
+    id: 6,
+    img: traffic,
+    title: "Traffic",
+    description: "Energetic tracks to help you power through your commute.",
+  },
+];
